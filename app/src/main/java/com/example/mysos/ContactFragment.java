1e6e3a105aa5c80953f671097e8cef9109ae0bf4
xlli3 <xlli3@student.unimelb.edu.au>
2019-10-22 13:05:11 +1100

    add database

--- app/src/main/java/com/example/mysos/ContactFragment.java
+++ app/src/main/java/com/example/mysos/ContactFragment.java
@@ -1,13 +1,29 @@
 package com.example.mysos;
 
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import static android.widget.Toast.*;
 
 
 /**
@@ -15,8 +31,15 @@ import android.view.ViewGroup;
  */
 public class ContactFragment extends Fragment {
 
+    SQLiteDatabase.CursorFactory factory;
+    private static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "sample_database";
+    public static final String TABLE_NAME = "contact";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_PHONE = "phone";
 
     public ContactFragment() {
+
         // Required empty public constructor
     }
 
@@ -24,8 +47,44 @@ public class ContactFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_contact, container, false);
+        super.onCreate(savedInstanceState);
+        final MySOSDB db = new MySOSDB(this.getActivity());
+        final EditText editName = (EditText) rootView.findViewById(R.id.name1);
+        final EditText editPhone = (EditText) rootView.findViewById(R.id.phone1);
+        Button submitButton = (Button) rootView.findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String nameInput = editName.getText().toString();
+                String phoneInput = editPhone.getText().toString();
+                boolean isInserted = db.insertData(nameInput,phoneInput);
+                if (isInserted){
+                    showMessage("1");
+                }else{
+                    showMessage("0");
+                }
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact, container, false);
+        return rootView;
     }
 
+    private void showMessage(String message) {
+
+        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void saveToDB() {
+        MySOSDB database = new MySOSDB(getActivity());
+
+
+    }
+
+
+
 }
+
+
---
+++ app/src/main/java/com/example/mysos/MySOSDB.java
@@ -0,0 +1,49 @@
+package com.example.mysos;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+public class MySOSDB extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "sample_database";
+    public static final String TABLE_NAME = "contact";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_PHONE = "phone";
+
+
+    public MySOSDB(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_NAME + " (" +
+                COLUMN_NAME + " TEXT, " +
+                COLUMN_PHONE + " TEXT" + ")");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+
+    public boolean insertData(String name, String phone){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME,name);
+        values.put(COLUMN_PHONE,phone);
+        long result = db.insert(TABLE_NAME,null,values);
+        if (result==-1){
+            return false;
+        }else{
+            return true;
+        }
+
+    }
+
+}
+
